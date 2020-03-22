5e4ee8c83e10752cca2d2827c86c7b32117b8d9e
bigMeow <running@vip.163.com>
2020-03-22 16:09:06 +0800

    docs: 文档测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: 'Cheers-mp',
+  description: 'Almost 零配置小程序脚手架(゜-゜)つロ 干杯~',
+  base: '/cheers-mp/',
+  themeConfig: {
+    repo: 'bigmeow/cheers-mp',
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+    smoothScroll: true,
+    displayAllHeaders: true,
+    sidebar: [
+      {
+        title: '介绍',
+        path: '/introduce/',
+        sidebarDepth: 1,
+      },
+      {
+        title: '快速开始',
+        sidebarDepth: 1,
+        collapsable: false,
+        children: [
+          '/start/install',
+          '/start/create'
+        ]
+      },
+      {
+        title: '配置参考',
+        path: '/config/',
+      },
+      {
+        title: 'CLI服务',
+        path: '/cli-service/',
+      },
+    ]
+  }
+}

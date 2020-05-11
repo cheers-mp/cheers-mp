9747e32a14f50dac4ddc99313fa86f15b3df000e
lizong <running@vip.163.com>
2020-05-11 14:07:20 +0800

    style: 该死的行尾符

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,17 @@
 module.exports = {
   root: true,
-  plugins: [
-    "node",
-    "prettier",
-  ],
+  plugins: ["node", "prettier"],
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   env: {
     es6: true,
     node: true,
-    jest: true
+    jest: true,
   },
   rules: {
     "prettier/prettier": "error",
-    "no-empty": ["error", { "allowEmptyCatch": true }]
-  }
-}
+    "no-empty": ["error", { allowEmptyCatch: true }],
+  },
+};
--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 Almost 零配置小程序工程化脚手架,文档建设完善中...
 
-> ps: 正在用它开发项目，边踩坑边修复Bug,故写文档的速度可能很慢...
+> ps: 正在用它开发项目，边踩坑边修复 Bug,故写文档的速度可能很慢...
 
 ## 相关配套资源
+
 - 精巧强大的小程序路由：[cheers-mp-router](https://github.com/bigmeow/cheers-mp-router)
-- 在小程序中使用axios request：[axios-miniprogram-adapter](https://github.com/bigmeow/axios-miniprogram-adapter)
+- 在小程序中使用 axios request：[axios-miniprogram-adapter](https://github.com/bigmeow/axios-miniprogram-adapter)
 
 ## 催更、钉钉交流群：
 

4fbc6df50fb4c4552adf499edeef9d7c77f381f6
lizong <running@vip.163.com>
2020-05-15 11:50:36 +0800

    fix:  当ts paths配置了指向node_modules 中的库路径时，import 第三方库 编译后的路径不正确（针对有赞库的优化）

--- packages/cheers-mp-service/lib/gulp/ts.js
+++ packages/cheers-mp-service/lib/gulp/ts.js
@@ -28,11 +28,21 @@ function ts(opt, userOptions) {
   const sourcemapsWriteLazy = lazypipe().pipe(gulpSourcemaps.write);
 
   function compileTS() {
+    const aliasConfig = JSON.parse(JSON.stringify(tsProject.config));
+    // 如果 alias 中包含 node_modules 的库，则过滤掉不处理，不然编译后它会指向 node_modules 目录而不是小程序自身的 miniprogram_npm 目录
+    const paths = aliasConfig.compilerOptions.paths;
+    if (paths) {
+      for (let [k, v] of Object.entries(paths)) {
+        if (/node_modules/.test(v)) {
+          delete paths[k];
+        }
+      }
+    }
     // TODO 如果ts 的target 是es5，则开发模式下默认输出sourcemaps
     const env = gulpReplaces(resolveClientEnv());
     return gulp
       .src(`${opt.srcDir}/**/*.ts`, { since: gulp.lastRun(compileTS) })
-      .pipe(gulpAlias({ configuration: tsProject.config }))
+      .pipe(gulpAlias({ configuration: aliasConfig }))
       .pipe(gulpIf(productionSourceMap, sourcemapsInitLazy()))
       .pipe(tsProject())
       .pipe(env)

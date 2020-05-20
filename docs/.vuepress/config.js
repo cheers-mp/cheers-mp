21e4fba1ff383d2b3cb3760a37a86e32acadb3b0
lizong <running@vip.163.com>
2020-05-20 18:27:39 +0800

    docs: 介绍

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,37 +1,34 @@
 module.exports = {
-  title: 'Cheers-mp',
-  description: 'Almost 零配置小程序脚手架(゜-゜)つロ 干杯~',
-  base: '/cheers-mp/',
+  title: "Cheers-mp",
+  description: "Almost 零配置小程序脚手架(゜-゜)つロ 干杯~",
+  base: "/cheers-mp/",
   themeConfig: {
-    repo: 'bigmeow/cheers-mp',
+    repo: "bigmeow/cheers-mp",
     editLinks: true,
     // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！',
+    editLinkText: "帮助我们改善此页面！",
     smoothScroll: true,
     displayAllHeaders: true,
     sidebar: [
       {
-        title: '介绍',
-        path: '/introduce/',
-        sidebarDepth: 1,
+        title: "介绍",
+        path: "/introduce/",
+        sidebarDepth: 3,
       },
       {
-        title: '快速开始',
-        sidebarDepth: 1,
+        title: "快速开始",
+        sidebarDepth: 3,
         collapsable: false,
-        children: [
-          '/start/install',
-          '/start/create'
-        ]
+        children: ["/start/install", "/start/create"],
       },
       {
-        title: '配置参考',
-        path: '/config/',
+        title: "配置参考",
+        path: "/config/",
       },
       {
-        title: 'CLI服务',
-        path: '/cli-service/',
+        title: "CLI服务",
+        path: "/cli-service/",
       },
-    ]
-  }
-}
+    ],
+  },
+};

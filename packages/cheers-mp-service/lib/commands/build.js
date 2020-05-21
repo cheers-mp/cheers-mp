a391e53ca3c22d8837571deab59afefd363cd622
lizong <running@vip.163.com>
2020-05-21 18:41:10 +0800

    fix: 描述错误

--- packages/cheers-mp-service/lib/commands/build.js
+++ packages/cheers-mp-service/lib/commands/build.js
@@ -16,7 +16,7 @@ module.exports = (api, userOptions) => {
       description: "生产构建",
       usage: "cheers-mp-cli-service build [options] [entry]",
       options: {
-        "--mode": `指定 env 文件模式 (默认: development)`,
+        "--mode": `指定 env 文件模式 (默认: production)`,
         "--clean": "是否每次编译前先清空处理掉输出目录(默认: true)",
         "--watch": "开启监听模式,默认关闭",
         "--upload": "编译结束后是否自动调用开发者工具上传，上传的小程序可在后台设置为体验版，默认关闭",

7ac90f2fbda975a9bc3390d398ea44f79406faf3
lizong <harry.li@71360.com>
2020-11-23 13:17:22 +0800

    perf: 完善类型定义

--- packages/cheers-mp-service/types/ProjectOptions.d.ts
+++ packages/cheers-mp-service/types/ProjectOptions.d.ts
@@ -1,41 +1,101 @@
-interface OSSOptions {
-  type: "ALI" | "QINIU";
-  options: object;
+
+interface AliOSS {
+  /** 阿里云 */
+  type: "ALI",
+  options:{
+    accessKeyId: string
+    accessKeySecret: string
+    /** 存储空间 */
+    bucket: string
+    /** 地域， 参考https://help.aliyun.com/document_detail/31837.html?spm=a2c4g.11186623.2.16.749469cb7oGBE5#concept-zt4-cvy-5db */
+    region: string
+    /** 访问路径前缀，可自行定义值，一般可以取项目名称，注意不要有特殊符号存在*/
+    prefix: string
+    /** 访问域名(选填) */
+    domain: string,
+    /** 是否使用 https */
+    https: boolean,
+  }
 }
+
+interface QiniuOSS {
+  /** 七牛云 */
+  type: "QINIU",
+  options:{
+    accessKey: string,
+    secretKey: string,
+    /** 存储区域，参考 https://developer.qiniu.com/kodo/manual/1671/region-endpoint */
+    zone: string,
+    /** 空间名称*/
+    bucket: string,
+    /** 访问路径前缀，可自行定义值，一般可以取项目名称，注意不要有特殊符号存在*/
+    prefix: string,
+    /** 访问域名必填 */
+    domain: string,
+    /** 是否使用 https */
+    https: boolean
+  }
+}
+
+interface UcloudOSS {
+  /** ucloud 云 */
+  type: "UCLOUD",
+  options:{
+    accessKeyId: string,
+    secretAccessKey: string,
+    /** 空间/桶名称*/
+    bucket: string,
+    /** 访问路径前缀，可自行定义值，一般可以取项目名称，注意不要有特殊符号存在*/
+    prefix: string,
+    /** 空间/桶地址*/
+    endpoint: string,
+    /** 是否使用 https上传 */
+    sslEnabled: false
+  }
+}
+
+
 
 /** “硬编译”配置 */
 interface HardOptions {
-  /** 开发者工具安装目录 */
-  devToolsDir: string;
-  /** 开发者工具命令行使用的版本 */
-  version: string;
+  /** 硬编译 */
+  type: "hard";
+  options: {
+    /** 开发者工具安装目录 */
+    devToolsDir: string;
+    /** 开发者工具命令行使用的版本 */
+    version: string;
+  }
 }
 
 /** “软编译”配置 */
 interface SoftOptions {
-  /** 	小程序/小游戏项目的 appid（默认读取project.config.json中的appid字段） */
-  appid?: string;
-  /** 项目的路径（默认project.config.json所在目录路径） */
-  projectPath?: string;
-  /** 私钥，在获取项目属性和上传时用于鉴权使用(必填) */
-  privateKeyPath: string;
-  /** 项目的类型，有效值 miniProgram/miniProgramPlugin/miniGame/miniGamePlugin， 默认miniProgram */
-  type: "miniProgram" | "miniProgramPlugin" | "miniGame" | "miniGamePlugin";
-  /** 指定需要排除的规则 */
-  ignores: string[];
+  /** 软编译 */
+  type: "soft";
+  options: {
+    /** 	小程序/小游戏项目的 appid（默认读取project.config.json中的appid字段） */
+    appid?: string;
+    /** 项目的路径（默认project.config.json所在目录路径） */
+    projectPath?: string;
+    /** 私钥，在获取项目属性和上传时用于鉴权使用(必填) */
+    privateKeyPath: string;
+    /** 项目的类型，有效值 miniProgram/miniProgramPlugin/miniGame/miniGamePlugin， 默认miniProgram */
+    type: "miniProgram" | "miniProgramPlugin" | "miniGame" | "miniGamePlugin";
+    /** 指定需要排除的规则 */
+    ignores: string[];
+  }
 }
 
-/** 编译器选项 */
-interface CompilerOptions {
-  /** 编译类型，“硬编译”,"软编译" */
-  type: "hard" | "soft";
-  options: HardOptions | SoftOptions;
-}
+type CompilerOptions = HardOptions | SoftOptions;
+
 export interface ProjectOptions {
-  oss?: OSSOptions;
+  /** 编译器选项 */
+  compiler: CompilerOptions,
+  /** 图片上传到云存储配置 */
+  oss?: AliOSS | QiniuOSS | UcloudOSS;
 
   lintOnSave?: boolean | "default" | "warning" | "error";
-
+  /** 插件配置选项 */
   pluginOptions?: object;
 }
 
